dd9e901f2638b5532937731df29757078e04586e
shashankreddyy <53851631+shashankreddyy@users.noreply.github.com>
2020-09-29 17:35:42 +0530

    Add files via upload

---
+++ Lab1/Lab1.R
@@ -0,0 +1,7 @@
+Lab 1 : create list of data objects
+
+N1<-c(10,20,30,40)
+Str1<-c("water","lemon","juice")
+Rlnum<-c(23.3,44.4,57.7,89.0)
+List1<-list(N1,Str1,Rlnum)
+List1
---
+++ Lab2/Lab2.R
@@ -0,0 +1,12 @@
+Lab2: Make vectors and join
+
+BuffTail<-c(10,1,37,5,12)
+Gardenbee<-c(8,3,9,6,4)
+RedTail<-c(18,9,12,4,6)
+Carderbee<-c(8,27,6,32,23)
+Honeybee<-c(12,13,16,9,10)
+beeframe<-data.frame(BuffTail,Gardenbee,RedTail,Carderbee,Honeybee)
+beeframe
+names<-c("Thristle","vipers","GoldenRain","yellowalfala","blackberry")
+rownames(beeframe)<-names
+beeframe
